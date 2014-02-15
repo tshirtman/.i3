c7fe8ce6485ca27ccff3a45f5099805e9cddc941
tshirtman <gabriel.pettier@gmail.com>
2014-02-15 18:48:05 +0100

    initial config

---
+++ get_workspace_options.sh
@@ -0,0 +1,2 @@
+#!/usr/bin/env sh
+i3-msg -t get_workspaces | jq -r 'map(.name)' | sed 's/[]"\[, ]//g;/^$/d'
---
+++ init.sh
@@ -0,0 +1,3 @@
+sudo apt-get install keynav acpi
+ln -sf $PWD/keynavrc ~/.keynavrc
+ln -sf $PWD/i3status.conf ~/.i3status.conf
