e20084247199d20b123524bf01b2e868b2a58fe2
gabriel pettier <dev@tangibledisplay.com>
2014-10-26 00:28:37 +0200

    move from i3status to i3blocks

--- init.sh
+++ init.sh
@@ -1,3 +1,13 @@
-sudo apt-get install keynav acpi jq i3
+#!/usr/bin/env sh
+# sudo apt-get install keynav acpi jq i3 pcmanfm lm-sensors sysstat
 ln -sf $PWD/keynavrc ~/.keynavrc
-ln -sf $PWD/i3status.conf ~/.i3status.conf
+ln -sf $PWD/i3blocks.conf ~/.i3blocks.conf
+ln -sf $PWD/fonts/* ~/.local/share/fonts/
+
+if [ ! -e ~/i3blocks ]
+then
+	git clone git://github.com/tshirtman/i3blocks ~/i3blocks
+	cd ~/i3blocks
+	make
+	cd -
+fi
