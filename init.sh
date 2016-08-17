b6ce775c511a061460e1511349e8c74d821abe72
gabriel pettier <dev@tangibledisplay.com>
2016-08-18 00:34:09 +0200

    fix file existence test

--- init.sh
+++ init.sh
@@ -14,7 +14,7 @@ then
 	cd -
 fi
 
-if [ -n -f /usr/local/bin/brightness ]; then
+if [ ! -e /usr/local/bin/brightness ]; then
 	sudo cp $HOME/.i3/brightness /usr/local/bin/
 fi
 echo 'please add the following rule to in "sudo visudo" if not present already'
