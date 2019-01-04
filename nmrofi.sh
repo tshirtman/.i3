5543fb9d578d661ac6e5f5acd81f32c65882d722
Gabriel Pettier <gabriel@tangibledisplay.com>
2019-01-04 21:22:32 +0100

    fixes to nmrofi.sh

--- nmrofi.sh
+++ nmrofi.sh
@@ -1,27 +1,19 @@
 #!/usr/bin/env bash
 
+target=$(nmcli device wifi|sed -s 1d|rofi -dmenu -p 📶| sed -s 's/^.\? *//'|sed -s 's/ *Infra.*//')
+
 connections() {
-    # list connection names, by sedding everything else out of the nmcli connections output
-    # use the UUID format to match what's not part of the name
-    nmcli connection \
-    |sed 1d\
-    |sed 's/^\(.*\) *[[:xdigit:]]\{8\}-[[:xdigit:]]\{4\}-[[:xdigit:]]\{4\}-[[:xdigit:]]\{4\}-[[:xdigit:]]\{12\}.*$/\1/'\
-    |sed 's/ *$//'
+	# list connection names, by sedding everything else out of the nmcli connections output
+	# use the UUID format to match what's not part of the name
+	nmcli connection \
+	|sed 1d\
+	|sed 's/^\(.*\) *[[:xdigit:]]\{8\}-[[:xdigit:]]\{4\}-[[:xdigit:]]\{4\}-[[:xdigit:]]\{4\}-[[:xdigit:]]\{12\}.*$/\1/'\
+	|sed 's/ *$//'
 }
 
-connect () {
-    target=$1
-    if [[ $(connections | grep -c "$target") -gt 0 ]]; then
-        message=$(nmcli connection up "$target")
-    else
-        message=$(nmcli device wifi connect "$target")
-    fi
-    notify-send "$message"
-}
-
-if [[ -z $@ ]]; then
-    nmcli device wifi|sed -s 1d
+if [[ $(connections | grep -c "$target") -gt 0 ]]; then
+	nmcli connection up "$target"
 else
-    target=$(echo "$@"| sed -s 's/^.\? *//'|sed -s 's/ *Infra.*//')
-    connect $target >& /dev/null &
+	nmcli device wifi connect "$target"
 fi
+
